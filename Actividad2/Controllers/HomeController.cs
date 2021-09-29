c41ad579e1b297f023a4296418ffbb0eac826258
Jacob Barrera <Jacob-bm@hotmail.com>
2021-09-29 11:33:45 -0500

    Agregar archivos de proyecto.

---
+++ Actividad2/Controllers/HomeController.cs
@@ -0,0 +1,23 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Actividad2.Models;
+
+namespace Actividad2.Controllers
+{
+    public class HomeController : Controller
+    {
+
+        public string Index()
+        {
+            return "Revisar Calificaciones";
+        }
+
+        public IActionResult Calificaciones(PerfilViewModel vm)
+        {
+                return View(vm);
+        }
+    }
+}
---
+++ Actividad2/Models/PerfilViewModel.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Actividad2.Models
+{
+    public class PerfilViewModel
+    {
+        public double Numero1 { get; set; }
+        public double Numero2 { get; set; }
+        public double Numero3 { get; set; }
+        public double Resultado {get { return (Numero1 + Numero2 + Numero3) / 3;  } }
+
+        public string Estado { get { return Resultado <= 100 && Resultado >= 70 ? "Aprobado" : "Reprobado"; } }
+    }
+}
